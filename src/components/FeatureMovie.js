874d7b6eefaa80202e7dfff0a5c8da4b6061f770
Lucca <luccag.barreto@gmail.com>
2022-03-05 23:29:11 -0300

    manipulação de informações na tela principal feitos e css do topo alterado

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -2,13 +2,38 @@ import React from "react";
 import './FeatureMovie.css';
 
 export default ({item}) => {
+        console.log(item);
+
+        // variável para manipular o ano exato(informação escolhida no https://www.themoviedb.org)
+        let firstDate = new Date(item.first_air_date);
+        // variável para recolher todos os gêneros do filme(informações da API https://www.themoviedb.org)
+        let genres = [];
+        for(let i in item.genres) {
+                genres.push(item.genres[i].name);
+        }
+
         return (
                 <section className="featured" style={{
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                         backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
                 }}>
-                        <div>{item.original_name}</div>
+                        <div className="feartured--vertical">
+                                <div className="feartured--horizontal">
+                                        <div className="featured--name">{item.original_name}</div>
+                                        <div className="featured--info">
+                                                <div className="featured--points">{item.vote_average} pontos</div>
+                                                <div className="featured--year">{firstDate.getFullYear()}</div>
+                                                <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                                        </div>
+                                        <div className="featured--description">{item.overview}</div>
+                                        <div className="featured--buttons">
+                                                
+                                        </div>
+                                        <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
+                                </div>
+
+                        </div>
                 </section>
         );
 }
